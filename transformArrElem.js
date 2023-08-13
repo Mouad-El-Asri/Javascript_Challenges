4cdeca3be4c1a95d0c4bca930423f8d3b207a8ef
Mouad El Asri <elasri.mouad.2002@gmail.com>
2023-08-13 18:55:01 +0100

    transformArrElements function

---
+++ transformArrElem.js
@@ -0,0 +1,46 @@
+/*
+	Given an integer array arr and a mapping function fn,
+	return a new array with a transformation applied to each element.
+*/
+
+var map = function(arr, fn) {
+    let transformedArr = [];
+    for (let i = 0; i < arr.length; i++)
+    {
+        transformedItem = fn(arr[i], i);
+        transformedArr[i]= transformedItem;
+    }
+    return (transformedArr);
+};
+
+
+//test
+arr = [1,2,3];
+fn = function plusI(n, i) { return n + i; };
+console.log(map(arr, fn));
+
+/*
+	Example 1:
+
+	Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+	Output: [2,3,4]
+	Explanation:
+	const newArray = map(arr, plusone); // [2,3,4]
+	The function increases each value in the array by one.
+
+	-------------------------------------------------------------------------
+
+	Example 2:
+
+	Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+	Output: [1,3,5]
+	Explanation: The function increases each value by the index it resides in.
+
+	-------------------------------------------------------------------------
+
+	Example 3:
+
+	Input: arr = [10,20,30], fn = function constant() { return 42; }
+	Output: [42,42,42]
+	Explanation: The function always returns 42.
+*/
